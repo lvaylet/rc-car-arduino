436abc24b7e3de63490295ce59b7dae55c64ac9f
Laurent Vaylet <lvaylet@users.noreply.github.com>
2018-11-02 11:09:36 +0100

    pack multiple PPM signals within single 20ms frame

--- rc-car-writer/rc-car-writer.ino
+++ rc-car-writer/rc-car-writer.ino
@@ -1,23 +1,38 @@
-int throttlePin = 9;     // pin attached to throttle (DC motor)
-int steeringPin = 10;    // pin attached to steering (servo motor)
-int throttlePos = 1500;  // initial values for throttle and steering
+/**
+ * Writing PPM signals to Electronic Speed Controller (ESC) with Arduino.
+ *
+ * References:
+ * - https://electronics.stackexchange.com/questions/129961/how-to-get-the-pwm-frequency-and-duration-of-each-pulse
+ *
+ * TODO
+ * - Refactor duplicate code with generate_ppm(pin, pos) function
+ */
+
+// Pins attached to DC motor (throttle) and servo motor (steering)
+int throttlePin = 9;
+int steeringPin = 10;
+
+// Initial positions for throttle and steering (= pulse width, in microseconds, usually between 1000 and 2000)
+int throttlePos = 1500;
 int steeringPos = 1500;
 
-void setup() {                
+void setup() {
   pinMode(throttlePin, OUTPUT);
   pinMode(steeringPin, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(steeringPin, HIGH);       // start PPM pulse
-  delayMicroseconds(steeringPos);        // wait pulse diration
-  digitalWrite(steeringPin, LOW);        // complete the pulse
-  // IMPORTANT
-  // delayMicroseconds() is limited to 16383µs
-  // Reference: http://arduino.cc/en/Reference/DelayMicroseconds
-  // Hence:
-  // delayMicroseconds(20000L-steeringPos);
-  // is replaced with:
-  delayMicroseconds(5000L-steeringPos);  // adjust to 5ms period
-  delay(15);                             // pad out 5ms to 20ms PPM period
+  // Pack the two PPM signals for throttle and steering within a single 20ms frame
+  
+  // Steering
+  digitalWrite(steeringPin, HIGH);        // start PPM pulse
+  delayMicroseconds(steeringPos);         // wait pulse duration
+  digitalWrite(steeringPin, LOW);         // complete pulse
+  delayMicroseconds(10000L-steeringPos);  // pad out to 10ms
+
+  // Throttle
+  digitalWrite(throttlePin, HIGH);        // start PPM pulse
+  delayMicroseconds(throttlePos);         // wait pulse duration
+  digitalWrite(throttlePin, LOW);         // complete pulse
+  delayMicroseconds(10000L-throttlePos);  // pad out to 10ms
 }
