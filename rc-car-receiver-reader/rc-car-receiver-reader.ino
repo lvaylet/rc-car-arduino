ad8a568b49689a00ea685f9fc21d21598cd57917
Laurent Vaylet <laurent.vaylet@bruitparif.fr>
2018-11-08 22:08:22 +0100

    remove unused channels

--- rc-car-receiver-reader/rc-car-receiver-reader.ino
+++ rc-car-receiver-reader/rc-car-receiver-reader.ino
@@ -23,17 +23,13 @@
 // How often to write values to serial port, in milliseconds?
 #define OUTPUT_PERIOD_MSEC  50
 
-#define RC_NUM_CHANNELS  4
+#define RC_NUM_CHANNELS  2
 
 #define RC_CH1  0
 #define RC_CH2  1
-#define RC_CH3  2
-#define RC_CH4  3
 
 #define RC_CH1_INPUT  A0
 #define RC_CH2_INPUT  A1
-#define RC_CH3_INPUT  A2
-#define RC_CH4_INPUT  A3
 
 auto timer = timer_create_default(); // create a timer with default settings
 
@@ -74,16 +70,12 @@ void calc_input(uint8_t channel, uint8_t input_pin) {
 
 void calc_ch1() { calc_input(RC_CH1, RC_CH1_INPUT); }
 void calc_ch2() { calc_input(RC_CH2, RC_CH2_INPUT); }
-void calc_ch3() { calc_input(RC_CH3, RC_CH3_INPUT); }
-void calc_ch4() { calc_input(RC_CH4, RC_CH4_INPUT); }
 
 bool write_rc_values_to_serial(void *) {
   read_rc_values();
   
   Serial.print("CH1:"); Serial.print(rc_values[RC_CH1]); Serial.print("\t");
-  Serial.print("CH2:"); Serial.print(rc_values[RC_CH2]); Serial.print("\t");
-  Serial.print("CH3:"); Serial.print(rc_values[RC_CH3]); Serial.print("\t");
-  Serial.print("CH4:"); Serial.println(rc_values[RC_CH4]);
+  Serial.print("CH2:"); Serial.println(rc_values[RC_CH2]);
 
   return true; // keep timer active?
 }
@@ -93,14 +85,10 @@ void setup() {
 
   pinMode(RC_CH1_INPUT, INPUT);
   pinMode(RC_CH2_INPUT, INPUT);
-  pinMode(RC_CH3_INPUT, INPUT);
-  pinMode(RC_CH4_INPUT, INPUT);
 
   // Enable interrupts and set functions called on high to low, or vice versa
   enableInterrupt(RC_CH1_INPUT, calc_ch1, CHANGE);
   enableInterrupt(RC_CH2_INPUT, calc_ch2, CHANGE);
-  enableInterrupt(RC_CH3_INPUT, calc_ch3, CHANGE);
-  enableInterrupt(RC_CH4_INPUT, calc_ch4, CHANGE);
 
   // Configure timer with callback
   timer.every(OUTPUT_PERIOD_MSEC, write_rc_values_to_serial);
